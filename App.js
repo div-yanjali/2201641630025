057b585b75db2109672d7ad1e98a7db3dd759ef9
div-yanjali <143691107+div-yanjali@users.noreply.github.com>
2025-09-08 14:58:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+﻿import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Container, Button } from "@mui/material";
+import ShortenerPage from "./pages/ShortenerPage";
+import StatsPage from "./pages/StatsPage";
+import RedirectHandler from "./components/RedirectHandler";
+
+export default function App() {
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Affordmed URL Shortener
+          </Typography>
+          <Button color="inherit" component={Link} to="/">Shorten</Button>
+          <Button color="inherit" component={Link} to="/stats">Stats</Button>
+        </Toolbar>
+      </AppBar>
+      <Container sx={{ mt: 3 }}>
+        <Routes>
+          <Route path="/" element={<ShortenerPage />} />
+          <Route path="/stats" element={<StatsPage />} />
+          <Route path="/:shortcode" element={<RedirectHandler />} />
+        </Routes>
+      </Container>
+    </>
+  );
+}
